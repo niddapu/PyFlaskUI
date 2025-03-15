e84766c1f83089ac8df4de24b5f1da76153bcf95
niddapu <niddapu@gmail.com>
2025-03-15 14:19:53 +0000

    enter two inputs

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def home():
 @app.route('/execute', methods=['POST'])
 def execute_script():
     try:
-        user_input = request.form.get('userInput')
-        result = subprocess.run(['python', 'src/script.py', user_input], capture_output=True, text=True)
+        user_input = [request.form.get('fname'), request.form.get('lname')]  # Add many inputs
+        print(user_input)
+        result = subprocess.run(['python', 'src/script.py'] + user_input, capture_output=True, text=True)
         return render_template('result/result.html', output=result.stdout, error=result.stderr)
     except Exception as e:
         return {'error': str(e)}
--- src/script.py
+++ src/script.py
@@ -2,7 +2,9 @@ import sys
 import json
 
 if len(sys.argv) > 1:
-    parm = sys.argv[1]
+
+    fname = sys.argv[1]
+    lname = sys.argv[2]
 
     # Print JSON output
-    print(parm)
+    print(f"{fname}  {lname}")
