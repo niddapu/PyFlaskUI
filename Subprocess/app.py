63d32d987456722b99298f8e8fe1f854148bd6ad
Srinivas Niddapu (TMNA) <srinivas.niddapu@toyota.com>
2025-03-18 09:45:10 +0530

    subprocess

---
+++ Subprocess/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, Response
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    def generate():
+        process = subprocess.Popen(['ping', '-t', 'google.com'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        for line in iter(process.stdout.readline, ''):
+            yield line
+    return Response(generate(), mimetype='text/plain')
+
+if __name__ == '__main__':
+    app.run(debug=True)
